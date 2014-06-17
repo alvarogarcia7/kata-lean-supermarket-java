b99eca38e99facfb2c872ee90775f8f4b2a2544b
JR_Diaz+A_Garcia <NoEmail@example.com>
2014-06-17 19:55:31 +0200

    Sprint 3: discount for cherries (cerezas)

--- src/main/java/lean01/Cajero.java
+++ src/main/java/lean01/Cajero.java
@@ -3,11 +3,16 @@ package lean01;
 public class Cajero {
 
 	private int total = 0;
+	private int cerezas;
 
 	public void read(String string) {
 		if("Manzanas".equals(string)){
 			this.total+=100;
 		} else {
+			this.cerezas++;
+			if(this.cerezas % 2 == 0){
+				this.total -=20;
+			}
 			this.total+=75;
 		}
 		
--- src/test/java/lean01/CajeroTest.java
+++ src/test/java/lean01/CajeroTest.java
@@ -21,12 +21,22 @@ public class CajeroTest {
 	}
 
 	@Test
-	public void cajeroConUnaManzana_Should_ReadCerezaAs175() {
+	public void cajeroConUnaManzana_Should_ReadDosCerezaAs250Discount20Equals230() {
+		Cajero cajero = new Cajero();
+		cajero.read("Manzanas");
+		cajero.read("Cerezas");
+		cajero.read("Cerezas");
+		assertEquals(cajero.total(), 230);
+	}
+
+	@Test
+	public void cajeroEmpty_Should_ReadFourCerezaAs300Discount40Equals260() {
 		Cajero cajero = new Cajero();
 		cajero.read("Cerezas");
 		cajero.read("Cerezas");
-		cajero.read("Manzanas");
-		assertEquals(cajero.total(), 250);
+		cajero.read("Cerezas");
+		cajero.read("Cerezas");
+		assertEquals(cajero.total(), 260);
 	}
 
 }
