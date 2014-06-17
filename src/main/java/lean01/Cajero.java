73cb927ff0855145830e953ee6988c1f58af999c
JR_Diaz+A_Garcia <NoEmail@example.com>
2014-06-17 20:21:06 +0200

    Sprint 4

--- src/main/java/lean01/Cajero.java
+++ src/main/java/lean01/Cajero.java
@@ -1,35 +1,46 @@
 package lean01;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Cajero {
 
-	private static final int BANANA_PRICE = 150;
+	public static final int CHERRY_PRICE = 75;
+	public static final int CHERRY_DISCOUNT = 20;
+	public static final int BANANA_PRICE = 150;
+	public static final int BANANA_DISCOUNT = BANANA_PRICE;
 	private int total = 0;
 	private int cerezas;
 	private int bananas;
+	private static final List<String> appleLocalizations = Arrays.asList(new String[] { "Manzanas","Pommes", "Mele"});
 
 	public void read(String string) {
 		calculatePrice(string);
-		
 	}
 
 	private void calculatePrice(String string) {
-		if("Manzanas".equals(string)){
+		if(isApple(string)){
 			this.total+=100;
-		} else if("Banana".equals(string)) {
+		} else if("Bananas".equals(string)) {
 			this.total+=BANANA_PRICE;
 			this.bananas ++;
 			if(this.bananas % 2 == 0){
-				this.total -=150;
+				this.total -=BANANA_DISCOUNT;
 			}
 			
 		}else {
 			//TODO rename as "aplicaDescuento"
 			this.cerezas++;
 			if(this.cerezas % 2 == 0){
-				this.total -=30;
+				this.total -=CHERRY_DISCOUNT;
 			}
-			this.total+=75;
+			this.total+=CHERRY_PRICE;
 		}
+	}
+
+	private boolean isApple(String string) {
+		return appleLocalizations.contains(string);
 	}
 
 	public int total() {
--- src/test/java/lean01/CajeroTest.java
+++ src/test/java/lean01/CajeroTest.java
@@ -14,6 +14,19 @@ public class CajeroTest {
 	}
 
 	@Test
+	public void emptyCajero_Should_ReadPommes() {
+		Cajero cajero = new Cajero();
+		cajero.read("Pommes");
+		assertEquals(cajero.total(), 100);
+	}
+	@Test
+	public void emptyCajero_Should_ReadMele() {
+		Cajero cajero = new Cajero();
+		cajero.read("Mele");
+		assertEquals(cajero.total(), 100);
+	}
+
+	@Test
 	public void emptyCajero_Should_ReadCerezaAs75() {
 		Cajero cajero = new Cajero();
 		cajero.read("Cerezas");
@@ -21,28 +34,28 @@ public class CajeroTest {
 	}
 
 	@Test
-	public void cajeroConUnaManzana_Should_ReadDosCerezaAs250Discount30Equals220() {
+	public void cajeroConUnaManzana_Should_ReadDosCerezaMinusDiscount() {
 		Cajero cajero = new Cajero();
 		cajero.read("Manzanas");
 		cajero.read("Cerezas");
 		cajero.read("Cerezas");
-		assertEquals(cajero.total(), 220);
+		assertEquals(cajero.total(), 100+2*cajero.CHERRY_PRICE-1*cajero.CHERRY_DISCOUNT);
 	}
 
 	@Test
-	public void cajeroEmpty_Should_ReadFourCerezaAs300Discount60Equals240() {
+	public void cajeroEmpty_Should_ReadFourCerezaAs300MinusDiscount() {
 		Cajero cajero = new Cajero();
 		cajero.read("Cerezas");
 		cajero.read("Cerezas");
 		cajero.read("Cerezas");
 		cajero.read("Cerezas");
-		assertEquals(cajero.total(), 240);
+		assertEquals(cajero.total(), 4*Cajero.CHERRY_PRICE-2*cajero.CHERRY_DISCOUNT);
 	}
 
 	@Test
 	public void cajeroEmpty_Should_ReadOneBananaAndCharge150() {
 	Cajero cajero = new Cajero();
-	cajero.read("Banana");
+	cajero.read("Bananas");
 	assertEquals(cajero.total(), 150);
 	
 	}
@@ -50,9 +63,21 @@ public class CajeroTest {
 	@Test
 	public void cajeroEmpty_Should_ReadTwoBananaAndOnlyChargesOne() {
 		Cajero cajero = new Cajero();
-		cajero.read("Banana");
-		cajero.read("Banana");
+		cajero.read("Bananas");
+		cajero.read("Bananas");
 		assertEquals(cajero.total(), 150);
+	}
+	
+	@Test
+	public void cajeroEmpty_Should_ReadAcceptationTestIteration4() {
+		Cajero cajero = new Cajero();
+		cajero.read("Cerezas");
+		cajero.read("Pommes");
+		cajero.read("Cerezas");
+		cajero.read("Bananas");
+		cajero.read("Bananas");
+		cajero.read("Manzanas");
+		assertEquals(cajero.total(), 480);
 	}
 
 }
