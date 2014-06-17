2bbcc4d522d8cba7e7c81c18a94ef904739c6e5e
JR_Diaz+A_Garcia <NoEmail@example.com>
2014-06-17 19:31:38 +0200

    sprint 0

---
+++ src/main/java/lean01/TPVReader.java
@@ -0,0 +1,19 @@
+package lean01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class TPVReader {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while(true){
+		input = bis.readLine();
+		System.out.println(input);
+		}
+	}
+
+}
